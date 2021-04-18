3d0c46b39b0012f57914fe702e4b9c8dbb8ce055
igorlesnenko <alan-desu@yandex.ru>
2021-04-18 17:50:18 +0400

    Exchange details

--- src/components/Exchange.js
+++ src/components/Exchange.js
@@ -1,26 +1,48 @@
 import * as React from 'react';
+import axios from 'axios';
+import { Link } from 'gatsby';
 import SEO from './seo';
+import { apiUrl } from '../../config';
 
-export const Exchange = () => {
+export const Exchange = ({ id }) => {
   const [exchange, setExchange] = React.useState(null);
-
-  React.useEffect(() => {
-    setExchange({ name: 'test' });
+  React.useEffect(async () => {
+    const { data } = await axios.get(`${apiUrl}/exchanges/${id}`);
+    setExchange(data);
   }, []);
 
   if (exchange === null) {
-    return (
-      <>
-        <SEO title="Exchange" />
-        <h2>Loading exchange...</h2>
-      </>
-    );
+    return <h2>Loading exchange...</h2>;
   }
 
   return (
     <>
-      <SEO title="Exchange" />
+      <SEO title={`Exchange ${exchange.name}`} />
+      <div
+        style={{
+          marginBottom: '20px',
+        }}
+      >
+        <Link to="/">← Back to main page</Link>
+      </div>
+      <div>
+        <img src={exchange.image} alt={exchange.name} />
+      </div>
       <div>{exchange.name}</div>
+      <div>{exchange.country}</div>
+      <div>
+        Trust score:
+        {exchange.trust_score}
+      </div>
+      <div>
+        Year established:
+        {exchange.year_established}
+      </div>
+      {exchange.facebook_url !== '' && (
+        <div>
+          <a href={exchange.facebook_url}>{exchange.facebook_url}</a>
+        </div>
+      )}
     </>
   );
 };
--- src/pages/exchange.js
+++ src/pages/exchange.js
@@ -2,9 +2,11 @@ import * as React from 'react';
 import { Router } from '@gatsbyjs/reach-router';
 import Layout from '../components/layout';
 import { Exchange } from '../components/Exchange';
+import SEO from '../components/seo';
 
 const ExchangePage = () => (
   <Layout>
+    <SEO title="Exchange" />
     <Router>
       <Exchange path="/exchange/:id" component={Exchange} />
     </Router>
