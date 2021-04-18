b89ee7f2f3f287065d798b9cc7f7cbb0fb471045
igorlesnenko <alan-desu@yandex.ru>
2021-04-18 16:50:21 +0400

    Fetch exchanges

---
+++ config.js
@@ -0,0 +1,1 @@
+export const apiUrl = 'https://api.coingecko.com/api/v3';
--- src/components/Exchanges.js
+++ src/components/Exchanges.js
@@ -1,10 +1,17 @@
 import * as React from 'react';
+import axios from 'axios';
+import { apiUrl } from '../../config';
 
 export const Exchanges = ({ page }) => {
   const [exchanges, setExchanges] = React.useState(null);
+  const [total, setTotal] = React.useState(null);
 
-  React.useEffect(() => {
-    setExchanges([{ name: 123 }]);
+  React.useEffect(async () => {
+    const { data, headers } = await axios.get(
+      `${apiUrl}/exchanges?per_page=10&page=${page ?? 1}`,
+    );
+    setExchanges(data);
+    setTotal(headers.total);
   }, []);
 
   if (exchanges === null) {
@@ -14,6 +21,8 @@ export const Exchanges = ({ page }) => {
   return (
     <>
       {page}
+      {' '}
+      {total}
       <ul>
         {exchanges.map((exchange) => (
           <li key={exchange.id}>{exchange.name}</li>
